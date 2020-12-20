bd19c6f561b92e8e571122f78143e4dca08d86db
JuanjoCG <juanjo@arch.localdomain>
2020-12-20 16:16:40 +0100

    V1 finished, with no color

--- ipShow.py
+++ ipShow.py
@@ -4,6 +4,7 @@ import os
 import sys
 import requests
 import json
+from colorama import Fore, Back, Style
 
 # getting api urls
 get_api = requests.get("https://pastebin.com/raw/ZLiLZjMC")
@@ -21,12 +22,12 @@ def getIP():    # perform request to get public real ip in json format
     request = requests.get(api_url_list[0])
     return json.loads(json.dumps(request.json()))
 
-def getIPInfo(ip):        # perform request to get info about ip
+def getIPInfo(ip):  # perform request to get info about ip
     api_url_list[1] = str(api_url_list[1] + str(ip))
     request = requests.get(api_url_list[1])
     return json.loads(json.dumps(request.json()))
 
-def torDetector(ip):
+def torDetector(ip):    # search in database if IP is a tor exit node
     request = requests.get(api_url_list[2])
     tor_exit_nodes_list = request.text.split()
 
@@ -38,55 +39,58 @@ def torDetector(ip):
         return None
 
 def main():     # main method
-    try:
 
-        json_ip = getIP()
-        json_ip_info = getIPInfo(json_ip["ip"]) 
+    json_ip = getIP()
+    json_ip_info = getIPInfo(json_ip["ip"]) 
        
-# -------------- Printing IP ----------------------------------
+#-------------- Printing IP ----------------------------------
         
-        print(" ")
-        print("IP" + " --> " + json_ip["ip"])
+    print(" ")
+    print("IP" + " --> " + json_ip["ip"])
 
 # --------------- Printing IP info -----------------------------
-        keys_list = []
-        values_list = []
+    keys_list = []
+    values_list = []
 
-        for item in json_ip_info:
-            keys_list.append(str(item))
+    for item in json_ip_info:
+        keys_list.append(str(item))
         
-        for item in json_ip_info.values():
-            values_list.append(item)
+    for item in json_ip_info.values():
+        values_list.append(item)
        
-        i = 0
-        for item in range(len(keys_list)):
-            print(str(keys_list[i]) + " --> " + str(values_list[i]))
-            i = i + 1
+    i = 0
+    for item in range(len(keys_list)):
+        print(str(keys_list[i]) + " --> " + str(values_list[i]))
+        i = i + 1
+
+        if i == 13:
+            break
 
 # ---------------- Checking if IP is in tor exit node database ---
-        print(" ")
-        print("Tor IP" + " --> " + str(torDetector(json_ip["ip"])))
+    print(" ")
+    print("Tor exit node" + " --> " + str(torDetector(json_ip["ip"])))
 
-# ----------------------------------------------------------------
-                # Defining exceptions
-    except requests.exceptions.ConnectionError:
-        print("ConnectionError - Check internet connection:(")
-        print("please check your interent connection")
-        sys.exit()
-    except requests.exceptions.HTTPError:
-        print("HTTPError -- API is not sending valid requests :(")
-        sys.exit()
-    except requests.exceptions.ProxyError:
-        print("ProxyError -- Proxy is refusing connections :(")
-        sys.exit()
-    except requests.exceptions.SSLError:
-        print("SSLError  :(")
-        sys.exit()
-    except requests.exceptions.Timeout:
-        print("Timeout - Connection with server expired :(")
-        sys.exit()
+#----------------------- Calling main method ----------------------
+
+try:
+
+    main()  # Here main is called
 
 
-# -------------------------------------------------------
+except requests.exceptions.ConnectionError:
+    print("ConnectionError - Check internet connection:(")
+    print("please check your interent connection")
+    sys.exit()
+except requests.exceptions.HTTPError:
+    print("HTTPError -- API is not sending valid requests :(")
+    sys.exit()
+except requests.exceptions.ProxyError:
+    print("ProxyError -- Proxy is refusing connections :(")
+    sys.exit()
+except requests.exceptions.SSLError:
+    print("SSLError  :(")
+    sys.exit()
+except requests.exceptions.Timeout:
+    print("Timeout - Connection with server expired :(")
+    sys.exit()
 
-main()          # Here main is called
